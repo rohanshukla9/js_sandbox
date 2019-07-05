476bd05463f026c1f28423f6d48e10d79579d04c
Rohan <voidrohan@gmail.com>
2019-07-05 19:03:12 +0530

    removing client

--- projects/oop_es6/githubsearch/github.js
+++ projects/oop_es6/githubsearch/github.js
@@ -1,20 +1,25 @@
-
 class Github {
-
-  constructor(){
-    this.client_id = 'e35a7452a66ed93d3c95';
-    this.client_secret = '91f7424a24956342c553de55e32d3a4540e59d6d';
+  constructor() {
+    this.client_id = 'client_id_goes_here';
+    this.client_secret = 'insert_client_Secret_here';
     this.repos_count = 5;
     this.repos_sort = 'created: asc';
   }
 
-  async getUser(user){
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
 
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-   
-
-    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}/`);
-
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }/`
+    );
 
     const profile = await profileResponse.json();
     const repos = await repoResponse.json();
@@ -22,7 +27,6 @@ class Github {
     return {
       profile,
       repos
-    }
-
+    };
   }
 }
